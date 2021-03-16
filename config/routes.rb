70b19b16af32351e521baac6120ae133fa81611d
David Moina <davemolina777@gmail.com>
2021-03-16 13:32:34 -0500

    commit tree d4d13c8f4428b38f712fafa9e8add00d48f24bed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:show, :new, :edit, :update, :create]
+  resources :posts, only: [:show, :new, :create, :edit, :update, :create]
 end
